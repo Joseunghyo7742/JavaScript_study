d1761b67321ed9fb27e0085d7e6e5084ec2600f7
joseunghyo <seunghyo7742@naver.com>
2021-07-02 16:59:07 +0900

    add paintToDo func for adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,18 @@ const toDoList=document.getElementById("todo-list");
 const toDoInput=toDoForm.querySelector("input");
 //const toDoInput=document.querySelector("#todo-form input"); 과 동일
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span= document.createElement("span");
+    li.appendChild(span);
+    span.innerText=newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
+    paintToDo(newTodo);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
