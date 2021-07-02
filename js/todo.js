0d3032c53a84c694c28e130f591aae6008828202
joseunghyo <seunghyo7742@naver.com>
2021-07-02 17:15:55 +0900

    add deleteTodo func for delete ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,20 @@ const toDoForm=document.getElementById("todo-form");
 const toDoList=document.getElementById("todo-list");
 const toDoInput=toDoForm.querySelector("input");
 //const toDoInput=document.querySelector("#todo-form input"); 과 동일
+function deleteTodo(event){
+    const li= event.target.parentElement;
+    li.remove();
+}
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span= document.createElement("span");
-    li.appendChild(span);
     span.innerText=newTodo;
+    const button= document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click",deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
